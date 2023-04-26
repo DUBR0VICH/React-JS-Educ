8aceeb400acc9f9e8bcc4c1ea6ecca62a8e13aa0
DUBR0VICH <andrey.dubrov.04@mail.ru>
2023-04-26 14:52:31 +0300

    Отправить данные на сервер не получилось
    Я пытался отправить данные на сервер, который сделан через Express.js, но у меня что-то не срослось.

--- www/src/components/Register.js
+++ www/src/components/Register.js
@@ -1,18 +1,33 @@
 import React from "react"
-
+import { connect } from "react-redux"
+import { addUser } from "../reducers/actions"
 
 class Register extends React.Component {
+  
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const user = {
+      username: event.target.elements.username.value,
+      name: event.target.elements.name.value,
+      email: event.target.elements.email.value,
+      address: event.target.elements.address.value,
+      zipcode: event.target.elements.zipcode.value,
+      geolocation: event.target.elements.geolocation.value
+    }
+    this.props.addUser(user);
+  }
+
   render() {
     return(
       <div className="Reg">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <h3 className="Regist">Registration form</h3>
-          <p className="Reg1">Username: <input placeholder="Your Username" /></p>
-          <p className="Reg1">Name: <input placeholder="Your Name" /></p>
-          <p className="Reg1">Email: <input placeholder="Your Email" /></p>
-          <p className="Reg1">Address: <input placeholder="Your address" /></p>
-          <p className="Reg1">Zipcode: <input placeholder="Your zipcode" /></p>
-          <p className="Reg1">Geolocation: <input placeholder="Your geolocation" /></p>
+          <p className="Reg1">Username: <input name="username" placeholder="Your Username" /></p>
+          <p className="Reg1">Name: <input name="name" placeholder="Your Name" /></p>
+          <p className="Reg1">Email: <input name="email" placeholder="Your Email" /></p>
+          <p className="Reg1">Address: <input name="address" placeholder="Your address" /></p>
+          <p className="Reg1">Zipcode: <input name="zipcode" placeholder="Your zipcode" /></p>
+          <p className="Reg1">Geolocation: <input name="geolocation" placeholder="Your geolocation" /></p>
           <button type="submit" className="btn"> Submit </button>
         </form>
       </div>
@@ -20,4 +35,4 @@ class Register extends React.Component {
   }
 }
 
-export default Register
+export default connect(null, { addUser })(Register);
--- www/src/index.js
+++ www/src/index.js
@@ -2,7 +2,13 @@ import React from "react"
 import * as ReactDOMClient from 'react-dom/client'
 import App from './App'
 import './css/main.css'
+import { Provider } from "react-redux"
+import store from "./reducers/store"
 
 const app= ReactDOMClient.createRoot(document.getElementById("app"))
 
-app.render(<App />)
+app.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
